a1b0f82ae0ac6e7d5c84f9546518f000e266c9f9
iJrM <44652718+iJrM@users.noreply.github.com>
2019-03-12 16:46:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "d";
 var adminprefix = '-M'
-const developers = [''] //الاي دى حقك
+const developers = ['251084318846353410'] //الاي دى حقك
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
